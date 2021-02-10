6c91cc341789b92e6212470628005bcebd3ba4f8
Samuel Maurice <samuel@acadelib.com>
2021-02-11 00:16:25 +0100

    Add service provider

---
+++ src/ModuleServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Acadelib\Modules;
+
+use Illuminate\Support\ServiceProvider;
+
+class ModuleServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
